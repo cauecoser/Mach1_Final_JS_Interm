c1bcebcfb1098a26530917581b1d7fed2804d5e7
Cauê Coser <cauecoser@gmail.com>
2023-03-15 23:07:28 -0300

    Correção do sistema de nomenclatura dos IDs das categorias.

--- categorias.js
+++ categorias.js
@@ -5,19 +5,24 @@ let botaoAddCancelAddCategoria = document.querySelector('#botaoCancelAddCategori
 let nomeCategoria = document.querySelector('#nomeCategoria')
 let botaoSalvarCategoria = document.querySelector('#botaoSalvarCategoria')
 let categorias = []
-let id = 0
+let id = ''
 let linkCategorias = document.querySelector('#linkCategorias')
 let telaHome = document.querySelector('#home')
 let cadCtaegorias = document.querySelector('#cadCategorias')
 let filtroCategoria = document.querySelector('#filtroCategoria')
 
-let data = new Date()
-let ano = data.getFullYear()
-let mes = (data.getMonth() + 1) < 10 ? `0${(data.getMonth() + 1)}` : (data.getMonth() + 1)
-let dia = data.getDate() < 10 ? `0${data.getDate()}` : data.getDate()
-let horas = data.getHours() < 10 ? `0${data.getHours()}` : data.getHours()
-let minutos = data.getMinutes() < 10 ? `0${data.getMinutes()}` : data.getMinutes()
-let segundos = data.getSeconds() < 10 ? `0${data.getSeconds()}` : data.getSeconds()
+
+function defineID() {
+    let data = new Date()
+    let ano = data.getFullYear()
+    let mes = (data.getMonth() + 1) < 10 ? `0${(data.getMonth() + 1)}` : (data.getMonth() + 1)
+    let dia = data.getDate() < 10 ? `0${data.getDate()}` : data.getDate()
+    let horas = data.getHours() < 10 ? `0${data.getHours()}` : data.getHours()
+    let minutos = data.getMinutes() < 10 ? `0${data.getMinutes()}` : data.getMinutes()
+    let segundos = data.getSeconds() < 10 ? `0${data.getSeconds()}` : data.getSeconds()
+
+    return `${ano}${mes}${dia}_${horas}${minutos}${segundos}`
+}
 
 function mostraModal() {
     addEditCategoria.classList.remove('esconde')
@@ -34,7 +39,7 @@ function addCategoria() {
         alert('[ERRO] O nome da categoria deve conter ao menos 2 caracteres que não devem ser espaços em branco.')
     } else {
         categoria.nome = nomeCategoria.value
-        categoria.id = `${ano}${mes}${dia}_${horas}${minutos}${segundos}`
+        categoria.id = defineID()
         categorias.push(categoria)
         addEditCategoria.classList.add('esconde')
         listarCategorias(categorias)
@@ -66,7 +71,6 @@ function listarCategorias(lista) {
                 </tr>
             `
         })
-        console.log(lista)
     }
     corpoTabelaCategorias.innerHTML = listaCategorias
 }
@@ -82,3 +86,5 @@ botaoCancelAddCategoria.addEventListener('click', escondeModal)
 botaoSalvarCategoria.addEventListener('click', addCategoria)
 linkCategorias.addEventListener('click', listarCategorias)
 filtroCategoria.addEventListener('keyup', filtraCategorias)
+
+listarCategorias(categorias)
