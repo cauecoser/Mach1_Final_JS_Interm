0b2d659c4a61591df90858adb4d5b3674c620c9e
Cauê Coser <cauecoser@gmail.com>
2023-03-23 00:37:03 -0300

    Incluindo sistem de Local Storage para as Despesas.

--- despesas.js
+++ despesas.js
@@ -3,9 +3,8 @@ let corpoTabelaDespesas = document.querySelector('#corpoTabelaDespesas')
 let stats = ''
 let corLinha = ''
 let linkDespesas = document.querySelector('#linkDespesas')
-// let listaDespesasLocalStorage = JSON.parse(localStorage.getItem('listaDespesasLocalStorage'))
-// let despesas = listaDespesasLocalStorage ?? []
-let despesas = []
+let listaDespesasLocalStorage = JSON.parse(localStorage.getItem('listaDespesasLocalStorage'))
+let despesas = listaDespesasLocalStorage ?? []
 let addDespesa = document.querySelector('#addDespesa')
 let botaoAdicionarDespesa = document.querySelector('#botaoAdicionarDespesa')
 let valorResumoAPagar = document.querySelector('#valorResumoAPagar')
@@ -53,81 +52,81 @@ function contaDespesasAtrasadas(data, tipo) {
     }
 }
 
-    function excluirDespesa(indice) {
-        if (confirm('Deseja realmente excluir a despesa?')) {
-            tabelaDespesas.find((obj, index) => {
-                if (indice == index) {
-                    tabelaDespesas.splice(index, 1)
-                    contaDespesasAtrasadas(obj.data, 'sub')
-                    montarTabelaDespesas(tabelaDespesas)
-                    msg('Despesa excluída com sucesso!', 'sucesso')
-                }
-            })
-        }
+function excluirDespesa(indice) {
+    if (confirm('Deseja realmente excluir a despesa?')) {
+        tabelaDespesas.find((obj, index) => {
+            if (indice == index) {
+                tabelaDespesas.splice(index, 1)
+                contaDespesasAtrasadas(obj.data, 'sub')
+                montarTabelaDespesas(tabelaDespesas)
+                msg('Despesa excluída com sucesso!', 'sucesso')
+            }
+        })
     }
+}
 
-    function filtraDespesas() {
-        let despesasFiltradas = tabelaDespesas.filter(despesa =>
-            despesa.data.includes(filtroDespesa.value) ||
-            despesa.nome.toLowerCase().trim().includes(filtroDespesa.value.toLowerCase().trim()) ||
-            despesa.valor.includes(filtroDespesa.value) ||
-            despesa.categoria.toLowerCase().trim().includes(filtroDespesa.value.toLowerCase().trim()) ||
-            despesa.status.toLowerCase().includes(filtroDespesa.value.toLowerCase())
-        )
-        montarTabelaDespesas(despesasFiltradas)
-    }
+function filtraDespesas() {
+    let despesasFiltradas = tabelaDespesas.filter(despesa =>
+        despesa.data.includes(filtroDespesa.value) ||
+        despesa.nome.toLowerCase().trim().includes(filtroDespesa.value.toLowerCase().trim()) ||
+        despesa.valor.includes(filtroDespesa.value) ||
+        despesa.categoria.toLowerCase().trim().includes(filtroDespesa.value.toLowerCase().trim()) ||
+        despesa.status.toLowerCase().includes(filtroDespesa.value.toLowerCase())
+    )
+    montarTabelaDespesas(despesasFiltradas)
+}
 
-    function testaData(dataDespesaTabela) {
-        let data = new Date()
-        let [anoDataTabela, mesDataTabela, diaDataTabela] = dataDespesaTabela.split('-')
-        let dataDespesa = new Date(anoDataTabela, (mesDataTabela - 1), diaDataTabela)
+function testaData(dataDespesaTabela) {
+    let data = new Date()
+    let [anoDataTabela, mesDataTabela, diaDataTabela] = dataDespesaTabela.split('-')
+    let dataDespesa = new Date(anoDataTabela, (mesDataTabela - 1), diaDataTabela)
 
-        return data >= dataDespesa
-    }
+    return data >= dataDespesa
+}
 
-    function mostraModalDespesas() {
-        telaHome.classList.add('esconde')
-        addDespesa.classList.remove('esconde')
-        categoriaSelecionada.focus()
-    }
+function mostraModalDespesas() {
+    telaHome.classList.add('esconde')
+    addDespesa.classList.remove('esconde')
+    categoriaSelecionada.focus()
+}
 
-    function escondeModalDespesas() {
-        telaHome.classList.remove('esconde')
-        addDespesa.classList.add('esconde')
-        filtroDespesa.focus()
-    }
+function escondeModalDespesas() {
+    telaHome.classList.remove('esconde')
+    addDespesa.classList.add('esconde')
+    filtroDespesa.focus()
+}
 
-    function montarTabelaDespesas(lista) {
-        escondeModalDespesas()
-        addEditCategoria.classList.add('esconde')
-        cadCategorias.classList.add('esconde')
-        telaHome.classList.remove('esconde')
+function montarTabelaDespesas(lista) {
+    escondeModalDespesas()
+    addEditCategoria.classList.add('esconde')
+    cadCategorias.classList.add('esconde')
+    telaHome.classList.remove('esconde')
 
-        let linhaDespesa = ''
-        let totalPago = 0
-        let totalAPagar = 0
+    let linhaDespesa = ''
+    let totalPago = 0
+    let totalAPagar = 0
 
-        if (lista.length == 0) {
+    if (lista.length == 0) {
 
-            linhaDespesa = `
+        linhaDespesa = `
         <tr>
         <td colspan='6'>Não existe despesa a ser exibida.</td>          
         </tr>
         `
-        } else {
-            lista.forEach((obj, index) => {
+    } else {
+        lista.forEach((obj, index) => {
 
-                if (obj.status == "PAGO") {
-                    stats = "btnPago"
-                    corLinha = "linhaVerde"
-                    totalPago += Number(obj.valor.replace('.', '').replace(',', '.'))
-                } else {
-                    stats = "btnPendente"
-                    corLinha = "linhaVermelha"
-                    totalAPagar += Number(obj.valor.replace('.', '').replace(',', '.'))
-                }
+            if (obj.status == "PAGO") {
+                stats = "btnPago"
+                corLinha = "linhaVerde"
+                totalPago += Number(obj.valor.replace('.', '').replace(',', '.'))
+            } else {
+                stats = "btnPendente"
+                corLinha = "linhaVermelha"
+                totalAPagar += Number(obj.valor.replace('.', '').replace(',', '.'))
+            }
 
-                linhaDespesa += `
+            linhaDespesa += `
             <tr>
             <td class="${corLinha}">${formataExibicaoDataDespesa(obj.data)}</td>
             <td class="${corLinha}">${obj.nome}</td>
@@ -137,115 +136,118 @@ function contaDespesasAtrasadas(data, tipo) {
             <td><input type="button" class="botao botaoCancel" value="EXCLUIR" onclick="excluirDespesa(${index})"></td>
             </tr>
             `
-            })
-            // atualizaDespesasLocalStorage(lista)
-        }
-
-        valorResumoAtrasadas.innerHTML = totalAtrasadas
-        corpoTabelaDespesas.innerHTML = linhaDespesa
-        valorResumoPagas.innerHTML = Number(totalPago).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-        valorResumoAPagar.innerHTML = Number(totalAPagar).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-    }
-
-    function atualizaDespesasLocalStorage(lista) {
-        localStorage.setItem('listaDespesasLocalStorage', JSON.stringify(lista))
-    }
-
-    function formataReal(campo, apenasNumeros) {
-        if (apenasNumeros) {
-            let aux = ''
-            aux = campo.replace(/\D/g, '')
-            aux = (aux / 100).toFixed(2) + ''
-            aux = aux.replace(".", ",")
-            aux = aux.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-            return aux
-        }
-        return `R$ ${campo}`
-    }
-
-    function trocaStatus(indice) {
-        tabelaDespesas.forEach((obj, index) => {
-            if (indice == index) {
-                if (obj.status == "PAGO") {
-                    obj.status = "PENDENTE"
-                    contaDespesasAtrasadas(obj.data, 'soma')
-                } else {
-                    obj.status = "PAGO"
-                    contaDespesasAtrasadas(obj.data, 'sub')
-                }
-            }
         })
-        montarTabelaDespesas(tabelaDespesas)
     }
+    atualizaDespesasLocalStorage(lista)
 
-    function validaDataVencimentoDespesa(data) {
-        if (`${data}`.length != 10) {
-            msg('[ERRO] A data informada não possui formato adequado (dd/mm/aaaa). Insira um formato válido para continuar.', 'falha')
-            return false
-        }
-        return true
+    valorResumoAtrasadas.innerHTML = totalAtrasadas
+    corpoTabelaDespesas.innerHTML = linhaDespesa
+    valorResumoPagas.innerHTML = Number(totalPago).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    valorResumoAPagar.innerHTML = Number(totalAPagar).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
+
+function atualizaDespesasLocalStorage(lista) {
+    localStorage.setItem('listaDespesasLocalStorage', JSON.stringify(lista))
+}
+
+function formataReal(campo, apenasNumeros) {
+    if (apenasNumeros) {
+        let aux = ''
+        aux = campo.replace(/\D/g, '')
+        aux = (aux / 100).toFixed(2) + ''
+        aux = aux.replace(".", ",")
+        aux = aux.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+        return aux
     }
+    return `R$ ${campo}`
+}
 
-    function formataExibicaoDataDespesa(data) {
-        anoExibicao = data.slice(0, 4)
-        mesExibicao = data.slice(5, 7)
-        diaExibicao = data.slice(8, 10)
-
-        return dataParaExibir = `${diaExibicao}/${mesExibicao}/${anoExibicao}`
-    }
-
-    function adicionarDespesa() {
-        escondeModalDespesas()
-
-        if (nomeDespesa.value.trim().length < 2) {
-            msg('[ERRO] O nome da despesa deve conter pelo menos 2 caracteres que não podem ser espaços em branco. Altere o nome da despesa para realizar a inclusão.', 'falha')
-            mostraModalDespesas()
-        } else if (valorDespesa.value.length == 0) {
-            msg('[ERRO] O valor da despesa deve ser preenchido!.', 'alerta')
-            mostraModalDespesas()
-        } else {
-            if (categorias.find(obj => obj.nome == categoriaSelecionada.value)) {
-
-                let despesa = {}
-
-                despesa.data = dataVencimentoDespesa.value
-                despesa.nome = nomeDespesa.value
-                despesa.valor = valorDespesa.value
-                despesa.categoria = categoriaSelecionada.value
-                despesa.status = "PENDENTE"
-
-                if (!validaDataVencimentoDespesa(despesa.data)) {
-                    mostraModalDespesas()
-                    return false
-                }
-
-                contaDespesasAtrasadas(despesa.data, 'soma')
-
-                tabelaDespesas.push(despesa)
-                montarTabelaDespesas(tabelaDespesas)
-                msg('Despesa incluída com sucesso!', 'sucesso')
+function trocaStatus(indice) {
+    tabelaDespesas.forEach((obj, index) => {
+        if (indice == index) {
+            if (obj.status == "PAGO") {
+                obj.status = "PENDENTE"
+                contaDespesasAtrasadas(obj.data, 'soma')
             } else {
-                if (confirm('[ERRO] Não é possível adicionar na despesa uma categoria que não esteja cadastrada. Cadastre a categoria desejada antes de inserir a despesa. Deseja adicionar a categoria?')) {
-                    mostraModalCategoria()
-                    nomeCategoria.value = categoriaSelecionada.value
-                } else {
-                    mostraModalDespesas()
-                }
+                obj.status = "PAGO"
+                contaDespesasAtrasadas(obj.data, 'sub')
+            }
+        }
+    })
+    montarTabelaDespesas(tabelaDespesas)
+}
+
+function validaDataVencimentoDespesa(data) {
+    if (`${data}`.length != 10) {
+        msg('[ERRO] A data informada não possui formato adequado (dd/mm/aaaa). Insira um formato válido para continuar.', 'falha')
+        return false
+    }
+    return true
+}
+
+function formataExibicaoDataDespesa(data) {
+    anoExibicao = data.slice(0, 4)
+    mesExibicao = data.slice(5, 7)
+    diaExibicao = data.slice(8, 10)
+
+    return dataParaExibir = `${diaExibicao}/${mesExibicao}/${anoExibicao}`
+}
+
+function adicionarDespesa() {
+    escondeModalDespesas()
+
+    if (nomeDespesa.value.trim().length < 2) {
+        msg('[ERRO] O nome da despesa deve conter pelo menos 2 caracteres que não podem ser espaços em branco. Altere o nome da despesa para realizar a inclusão.', 'falha')
+        mostraModalDespesas()
+    } else if (valorDespesa.value.length == 0) {
+        msg('[ERRO] O valor da despesa deve ser preenchido!.', 'alerta')
+        mostraModalDespesas()
+    } else {
+        if (categorias.find(obj => obj.nome == categoriaSelecionada.value)) {
+
+            let despesa = {}
+
+            despesa.data = dataVencimentoDespesa.value
+            despesa.nome = nomeDespesa.value
+            despesa.valor = valorDespesa.value
+            despesa.categoria = categoriaSelecionada.value
+            despesa.status = "PENDENTE"
+
+            if (!validaDataVencimentoDespesa(despesa.data)) {
+                mostraModalDespesas()
+                return false
+            }
+
+            contaDespesasAtrasadas(despesa.data, 'soma')
+
+            tabelaDespesas.push(despesa)
+            montarTabelaDespesas(tabelaDespesas)
+            msg('Despesa incluída com sucesso!', 'sucesso')
+        } else {
+            if (confirm('[ERRO] Não é possível adicionar na despesa uma categoria que não esteja cadastrada. Cadastre a categoria desejada antes de inserir a despesa. Deseja adicionar a categoria?')) {
+                mostraModalCategoria()
+                nomeCategoria.value = categoriaSelecionada.value
+            } else {
+                mostraModalDespesas()
             }
         }
     }
+}
 
 
-    linkDespesas.addEventListener('click', () => montarTabelaDespesas(tabelaDespesas))
-    botaoAdicionarDespesa.addEventListener('click', () => {
-        mostraModalDespesas()
-        insereOptionsCategorias()
-    })
-    botaoCancelarDespesa.addEventListener('click', escondeModalDespesas)
-    botaoSalvarDespesa.addEventListener('click', adicionarDespesa)
-    filtroDespesa.addEventListener('keyup', filtraDespesas)
-    valorDespesa.addEventListener('keyup', () => {
-        valorDespesa.value = formataReal(valorDespesa.value, true)
-    })
-
+linkDespesas.addEventListener('click', () => {
     montarTabelaDespesas(tabelaDespesas)
+    // atualizaCategoriasLocalStorage(tabelaDespesas)???
+})
+botaoAdicionarDespesa.addEventListener('click', () => {
+    mostraModalDespesas()
+    insereOptionsCategorias()
+})
+botaoCancelarDespesa.addEventListener('click', escondeModalDespesas)
+botaoSalvarDespesa.addEventListener('click', adicionarDespesa)
+filtroDespesa.addEventListener('keyup', filtraDespesas)
+valorDespesa.addEventListener('keyup', () => {
+    valorDespesa.value = formataReal(valorDespesa.value, true)
+})
+
+montarTabelaDespesas(tabelaDespesas)
